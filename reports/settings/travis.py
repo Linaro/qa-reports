37763ccaf8ca03851680a310eba60c8cffee8ddd
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-03-09 14:17:01 +0100

    Squashed commit of the following:
    commit b35da95c8c23595724d7e0828aafc63b0f73a20b
    Author: Sebastian Pawluś <sebastian.pawlus@gmail.com>
    Date:   Wed Mar 9 14:09:15 2016 +0100
    
        travis, adjust database settings
    
    commit 3a73d989e5fbae36d4cbd90ab69ac1979477f048
    Author: Sebastian Pawluś <sebastian.pawlus@gmail.com>
    Date:   Wed Mar 9 14:03:49 2016 +0100
    
        travis, manual tests git repo added

--- reports/settings/travis.py
+++ reports/settings/travis.py
@@ -6,9 +6,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'qa-reports',
-        'USER': 'qa-reports',
+        'USER': 'postgres',
         'PASSWORD': 'qa-reports',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
