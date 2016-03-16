fe9a1db70664ca52dc22dc058f5f7ccbd62a24a5
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-03-16 17:24:34 +0100

    fix, bad attribute invocation

--- reports/auth.py
+++ reports/auth.py
@@ -1,12 +1,12 @@
 from django.conf import settings
-from crowd.backend import CrowdBackend
+from crowd_auth.backend import CrowdBackend
 
 
 class Backend(CrowdBackend):
 
     def authenticate(self, username=None, password=None):
         user = super(Backend, self).authenticate(username, password)
-        if user.is_superuser():
+        if user.is_superuser:
             return user
         if user and user.groups.filter(name=settings.ACCESS_GROUP).exists():
             return user
