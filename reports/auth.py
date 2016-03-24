9fb575e086a096185efe8403fcfa880858e1cf3a
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-03-24 14:03:27 +0100

    auth, fix user check to prevent user=None

--- reports/auth.py
+++ reports/auth.py
@@ -6,8 +6,7 @@ class Backend(CrowdBackend):
 
     def authenticate(self, username=None, password=None):
         user = super(Backend, self).authenticate(username, password)
-        if user.is_superuser:
-            return user
-        if user and user.groups.filter(name=settings.ACCESS_GROUP).exists():
+        if user and (user.is_superuser or
+                     user.groups.filter(name=settings.ACCESS_GROUP).exists()):
             return user
         return None
